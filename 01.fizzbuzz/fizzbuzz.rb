e2fa9562b92703937dee7feca65cbbf0042c94c6
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-14 18:54:47 +0900

    lint: rubocop small a

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
-1.upto(20) { |i|
+1.upto(20) do |i|
   if i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz"
+    puts 'FizzBuzz'
   elsif i % 3 == 0
-    puts "Fizz"
+    puts 'Fizz'
   elsif i % 5 == 0
-    puts "Buzz"
+    puts 'Buzz'
   else
     puts i
   end
-}
+end
