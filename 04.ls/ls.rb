d67212874200a8efc10fedb4d4f85ff7ebdbce85
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-18 12:01:51 +0900

    定数を使用してlsプログラムの列数とギャップを設定し、コードを整理

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,24 +1,24 @@
 #!/usr/bin/env ruby
 
-col_num = 3
-gap = 2
+COL_NUM =3
+GAP = 2
 
 target_path = !ARGV[0].nil? ? ARGV[0] : "."
-target_path = target_path.delete_suffix('/')
+target_path = File.join(target_path, '*')
 
-item_list_with_path = Dir.glob("#{target_path}/*")
+item_list_with_path = Dir.glob(target_path)
 
 item_list = item_list_with_path.map{ |v| v.split('/').last}
 max_length = item_list.map{ |v| v.length}.max
 
-row_num = (item_list.length / col_num.to_f).ceil
+row_num = (item_list.length / COL_NUM.to_f).ceil
 
 row_num.times do |r|
   line = ""
-  col_num.times do |c|
+  COL_NUM.times do |c|
     item = item_list[r + row_num * c]
-    next unless !item.nil?
-    line << item.ljust(max_length + gap)
+    next if item.nil?
+    line << item.ljust(max_length + GAP)
   end
   puts line
 end
