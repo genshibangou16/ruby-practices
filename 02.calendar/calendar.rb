b145610371b3e3a1d3f40622fa1e252b87b2ec5a
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-19 21:27:01 +0900

    不要なコメントを削除

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,26 +3,14 @@
 
 require 'date'
 
-##
-# 取得
-##
-
-# 今日の日付の取得
 today = Date.today
 
-# 年と月だけ抽出しておく
 year = today.year
 month = today.month
 
-##
-# 判定
-##
-
-# 引数のチェック
 argv_year_index = ARGV.index('-y')
 argv_month_index = ARGV.index('-m')
 
-# 年、月の指定があれば今日から取得した値を上書き
 unless argv_year_index.nil?
   argv_year = ARGV[argv_year_index + 1]
   if argv_year.to_s.match?(/^\d+$/) && argv_year.to_i.between?(1, 9999)
@@ -43,15 +31,12 @@ unless argv_month_index.nil?
   end
 end
 
-# 「今日」に背景をつける
 day = (today.day if argv_month_index.nil? && argv_year_index.nil?)
 
-# 月初の曜日判定
 beginning_of_month = Date.parse("#{year}-#{month}-1")
 wday = beginning_of_month.wday
 wday -= 1
 
-# 月末の判定
 last_of_month = if month.to_i == 2
                   if ((year.to_i % 100).zero? && !(year.to_i % 400).zero?) || !(year.to_i % 4).zero?
                     28
@@ -64,16 +49,9 @@ last_of_month = if month.to_i == 2
                   31
                 end
 
-##
-# 書き出し
-##
-
-# 年月の書き出し
 puts "      #{month.to_s.rjust(2)}月 #{year}"
 
-# 曜日部分の書き出し
 puts '日 月 火 水 木 金 土'
-# カレンダー部分の書き出し
 6.times do |r|
   7.times  do |c|
     n = (r * 7) + c - wday
