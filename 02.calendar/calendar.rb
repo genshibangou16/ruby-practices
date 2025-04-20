4cd0f37d7b7d3b093b71a6de245267175ff08ee6
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-20 22:22:41 +0900

    カレンダー表示のロジックを簡素化し、月末日の計算を改善

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -22,29 +22,26 @@ end
 opt.parse(ARGV)
 
 beginning_of_month = Date.new(year, month, 1)
-last_of_month = beginning_of_month.next_month.prev_day.day
-
-wday = beginning_of_month.wday
+end_of_month = beginning_of_month.next_month.prev_day
 
 puts "      #{month.to_s.rjust(2)}月 #{year}"
 puts '日 月 火 水 木 金 土'
 
-6.times do |row|
-  7.times  do |col|
-    day = (row * 7) + col - wday + 1
-    if day > last_of_month
-      print "\n" if col != 0
-      exit
-    elsif day.positive?
-      if Date.new(year, month, day) == today
-        printf("\e[30;47m%2d\e[0m", day)
-      else
-        print day.to_s.rjust(2)
-      end
-    else
-      print '  '
-    end
-    print ' ' if col != 6
+lines = "   " * beginning_of_month.wday
+(beginning_of_month..end_of_month).each do |date|
+  if today == date
+    lines += "\e[30;47m#{date.day}\e[0m"
+  else
+    lines += date.day.to_s.rjust(2)
   end
-  print "\n"
+
+  if date.saturday?
+    lines += "\n"
+  else
+    lines += ' '
+  end
 end
+
+lines += "\n" if !end_of_month.saturday?
+
+print(lines)
