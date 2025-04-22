9d5a6a58277c4571a0f6155beacc46a71f6a569c
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-22 12:28:46 +0900

    Lsクラスを導入し、複数のパスを処理できるようにlsプログラムをリファクタリング

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,23 +4,37 @@
 COL_NUM = 3
 GAP = 2
 
-target_path = !ARGV[0].nil? ? ARGV[0] : '.'
-target_path = File.join(target_path, '*')
+target_paths = ARGV.empty? ? ['.'] : ARGV
 
-item_list_with_path = Dir.glob(target_path)
-
-item_list = item_list_with_path.map { |v| v.split('/').last }
-max_length = item_list.map(&:length).max
-
-row_num = (item_list.length / COL_NUM.to_f).ceil
-
-row_num.times do |r|
-  line = ''
-  COL_NUM.times do |c|
-    item = item_list[r + row_num * c]
-    next if item.nil?
-
-    line << item.ljust(max_length + GAP)
+class Ls
+  def initialize(path)
+    @path = path
   end
-  puts line
+
+  def print
+    target_path = File.join(@path, '*')
+    item_list_with_path = Dir.glob(target_path)
+    item_list = item_list_with_path.map { |i| i.split('/').last }
+    max_length = item_list.map(&:length).max
+    row_num = (item_list.length / COL_NUM.to_f).ceil
+
+    row_num.times do |row|
+      line = ''
+      COL_NUM.times do |col|
+        item = item_list[row + row_num * col]
+        next if item.nil?
+
+        line += item.ljust(max_length + GAP)
+      end
+      puts line
+    end
+  end
+end
+
+target_paths.map.with_index do |p, idx|
+  puts "\n" if target_paths.length > 1 && idx.positive?
+  puts "#{p}:\n" if target_paths.length > 1
+
+  ls = Ls.new(p)
+  ls.print
 end
