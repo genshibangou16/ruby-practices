82c91f79f6214fcce7e6677ac8226c26fcfdb200
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-04-23 12:16:27 +0900

    リファクタリング：Lsクラスを廃止し、puts_filesメソッドを導入してファイル出力処理を簡素化

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -6,35 +6,28 @@ GAP = 2
 
 target_paths = ARGV.empty? ? ['.'] : ARGV
 
-class Ls
-  def initialize(path)
-    @path = path
-  end
+def puts_files(path)
+  target_path = File.join(path, '*')
+  item_list_with_path = Dir.glob(target_path)
+  item_list = item_list_with_path.map { |i| i.split('/').last }
+  max_length = item_list.map(&:length).max
+  row_num = (item_list.length / COL_NUM.to_f).ceil
 
-  def print
-    target_path = File.join(@path, '*')
-    item_list_with_path = Dir.glob(target_path)
-    item_list = item_list_with_path.map { |i| i.split('/').last }
-    max_length = item_list.map(&:length).max
-    row_num = (item_list.length / COL_NUM.to_f).ceil
+  row_num.times do |row|
+    line = ''
+    COL_NUM.times do |col|
+      item = item_list[row + row_num * col]
+      next if item.nil?
 
-    row_num.times do |row|
-      line = ''
-      COL_NUM.times do |col|
-        item = item_list[row + row_num * col]
-        next if item.nil?
-
-        line += item.ljust(max_length + GAP)
-      end
-      puts line
+      line += item.ljust(max_length + GAP)
     end
+    puts line
   end
 end
 
-target_paths.map.with_index do |p, idx|
-  puts "\n" if target_paths.length > 1 && idx.positive?
-  puts "#{p}:\n" if target_paths.length > 1
+target_paths.each_with_index do |p, idx|
+  puts if target_paths.length > 1 && idx.positive?
+  puts "#{p}:" if target_paths.length > 1
 
-  ls = Ls.new(p)
-  ls.print
+  puts_files(p)
 end
