1aeb29c4bfcf8f6b315816e9aff95b7048e95004
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-05-21 12:17:39 +0900

    wcスクリプトの入力処理をリファクタリングし、変数名を改善

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -8,9 +8,9 @@ DEFAULT_LENGTH = 7
 def main
   params = parse_args
   target_paths = ARGV
-  strings_or_paths_data = parse_input_strings_or_paths(target_paths)
-  print_lines(strings_or_paths_data, params)
-  print_total(strings_or_paths_data, params) if strings_or_paths_data.length > 1
+  string_parse_results = parse_input_strings(target_paths)
+  print_lines(string_parse_results, params)
+  print_total(string_parse_results, params) if string_parse_results.length > 1
 end
 
 def parse_args
@@ -23,7 +23,7 @@ def parse_args
   params.values.any? ? params : { l: true, w: true, c: true }
 end
 
-def parse_input_strings_or_paths(target_paths)
+def parse_input_strings(target_paths)
   if target_paths.empty?
     [{
       path: nil,
@@ -53,18 +53,18 @@ def get_wc_data(str)
   }
 end
 
-def print_lines(strings_or_paths_data, params)
-  strings_or_paths_data.each do |string_or_path_data|
-    unless string_or_path_data[:file_exists]
-      puts "wc: #{string_or_path_data[:path]}: open: No such file or directory"
+def print_lines(string_parse_results, params)
+  string_parse_results.each do |string_parse_result|
+    unless string_parse_result[:file_exists]
+      puts "wc: #{string_parse_result[:path]}: open: No such file or directory"
       next
     end
-    if string_or_path_data[:is_directory]
-      puts "wc: #{string_or_path_data[:path]}: read: Is a directory"
+    if string_parse_result[:is_directory]
+      puts "wc: #{string_parse_result[:path]}: read: Is a directory"
       next
     end
 
-    print_line(string_or_path_data[:wc_data], string_or_path_data[:path], params)
+    print_line(string_parse_result[:wc_data], string_parse_result[:path], params)
   end
 end
 
@@ -83,12 +83,12 @@ def format_wc_data(data, params)
   end
 end
 
-def print_total(strings_or_paths, params)
+def print_total(strings, params)
   total = Hash.new(0)
-  strings_or_paths.each do |string_or_path|
-    next unless string_or_path[:wc_data]
+  strings.each do |string|
+    next unless string[:wc_data]
 
-    string_or_path[:wc_data].each { |k, v| total[k] += v.to_i }
+    string[:wc_data].each { |k, v| total[k] += v.to_i }
   end
   print_line(total.filter { |_, v| v.positive? }, 'total', params)
 end
