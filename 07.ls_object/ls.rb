055a6d91ef93c21bb7211402d7178ad0b6249aea
genshibangou16 <genshibangou16.rakkan@gmail.com>
2025-06-04 16:40:13 +0900

    Formatterクラスとそのサブクラス（LongFormatter、ShortFormatter）を追加し、lsコマンドの出力形式を改善

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -4,15 +4,14 @@
 require_relative 'command_line_options'
 require_relative 'file_entry'
 require_relative 'directory_entry'
+require_relative 'long_formatter'
+require_relative 'short_formatter'
 require 'etc'
 
 class Ls
-  COL_NUM = 3
-  GAP = 3
-
   def initialize(argv)
     @options = CommandLineOptions.new(argv)
-    paths = filter_paths(@options.paths)
+    paths, @invalid_paths = @options.paths.partition { |path| File.exist?(path) }
     paths.sort!
     paths.reverse! if @options.reverse
     dir_paths, file_paths = paths.partition { |path| File.directory?(path) }
@@ -21,85 +20,9 @@ class Ls
   end
 
   def print
-    if @options.long
-      print_long
-    else
-      print_short
-    end
-  end
-
-  private
-
-  def filter_paths(paths)
-    paths.map do |path|
-      if File.exist?(path)
-        path
-      else
-        puts "ls: #{path}: No such file or directory"
-      end
-    end.compact
-  end
-
-  def print_short
-    print_columns(@file_entries) unless @file_entries.empty?
-    puts if @file_entries.any? && @dir_entries.any?
-
-    @dir_entries.each_with_index do |directory, idx|
-      print_header(directory.path, idx) if @dir_entries.length + @file_entries.length > 1
-      print_columns(directory.file_entries)
-    end
-  end
-
-  def print_long
-    print_stats(@file_entries) unless @file_entries.empty?
-    puts if @file_entries.any? && @dir_entries.any?
-
-    @dir_entries.each_with_index do |directory, idx|
-      print_header(directory.path, idx) if @dir_entries.length + @file_entries.length > 1
-      puts "total #{directory.blocks}"
-      print_stats(directory.file_entries)
-    end
-  end
-
-  def print_columns(files)
-    paths = files.map(&:path)
-    col_width = paths.map(&:length).max + GAP
-    row_num = (paths.length / COL_NUM.to_f).ceil
-
-    row_num.times do |row|
-      line = (0...COL_NUM).map do |col|
-        idx = row + row_num * col
-        item = paths[idx]
-        item&.ljust(col_width)
-      end.compact.join
-      puts line
-    end
-  end
-
-  def print_header(path, idx)
-    puts unless idx.zero?
-    puts "#{path}:"
-  end
-
-  def print_stats(files)
-    max_lengths = {
-      nlink: files.map { |file| file.nlink.to_s.length }.max,
-      size: files.map { |file| file.size.to_s.length }.max,
-      uid: files.map { |file| file.uid.length }.max,
-      gid: files.map { |file| file.gid.length }.max
-    }
-    files.each do |file|
-      puts [
-        file.mode,
-        file.nlink.to_s.rjust(max_lengths[:nlink] + 1),
-        file.uid.ljust(max_lengths[:uid] + 1),
-        file.gid.ljust(max_lengths[:gid] + 1),
-        file.size.to_s.rjust(max_lengths[:size]),
-        file.mtime.strftime('%_m %e %H:%M'),
-        file.path,
-        file.symlink? ? "-> #{file.readlink}" : nil
-      ].compact.join(' ')
-    end
+    formatter_class = @options.long ? LongFormatter : ShortFormatter
+    formatter = formatter_class.new(dir_entries: @dir_entries, file_entries: @file_entries, invalid_paths: @invalid_paths)
+    puts formatter
   end
 end
 
---
+++ 07.ls_object/short_formatter.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative 'formatter'
+
+class ShortFormatter < Formatter
+  private
+
+  def format(files)
+    paths = files.map(&:path)
+    col_width = paths.map(&:length).max + GAP
+    row_num = (paths.length / COL_NUM.to_f).ceil
+
+    row_num.times do |row|
+      line = (0...COL_NUM).map do |col|
+        idx = row + row_num * col
+        item = paths[idx]
+        item&.ljust(col_width)
+      end.compact.join
+      @lines.push line
+    end
+  end
+end
